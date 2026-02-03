2c16bbecd165f2e4ccdfd188f119f40ac678dc11
leoo-c1 <chinchillaleo01@gmail.com>
2026-02-03 14:13:46 +1100

    Restructure

--- rtl/vga_renderer.v
+++ rtl/vga_renderer.v
@@ -1,4 +1,4 @@
-module vga_renderer (
+module fill_and_border (
     input clk_0,            // 25MHz clock
     input rst,              // Reset button
 
@@ -14,16 +14,28 @@ module vga_renderer (
     parameter h_video = 640;        // Horizontal active video (in pixels)
     parameter v_video = 480;        // Vertical active video (in lines)
 
-    // Render a white screen in the active video area, black outside
+    parameter border_width = 10;    // Width of the surrounding border
+
+    // Render a white screen in the active video area, red outside
     always @ (posedge clk_0) begin
         if (!rst) begin                 // If we press the reset button, show black
             red <= 1'b0;
             green <= 1'b0;
             blue <= 1'b0;
-        end else if (video_on) begin    // If we are in the active video region, show white
-            red <= 1'b1;
-            green <= 1'b1;
-            blue <= 1'b1;
+        end else if (video_on) begin    // Show white with red border in active video region
+            if (pixel_x < border_width || (pixel_x >= h_video - border_width && pixel_x < h_video)) begin
+                red <= 1'b1;
+                green <= 1'b0;
+                blue <= 1'b0;
+            end else if (pixel_y < border_width || (pixel_y >= v_video - border_width && pixel_y < v_video)) begin
+                red <= 1'b1;
+                green <= 1'b0;
+                blue <= 1'b0;
+            end else begin
+                red <= 1'b1;
+                green <= 1'b1;
+                blue <= 1'b1;
+            end
         end else begin                  // If we are outside the active video region, show black
             red <= 1'b0;
             green <= 1'b0;
